c29a910c7079f9393dcb8b223bc31a86e7646bc8
VihaanGupta124 <69687681+VihaanGupta124@users.noreply.github.com>
2021-03-31 16:26:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,9 +12,8 @@ var polygon_img, backgroundImg;
 var score=0;
 var bg = "images/light.jpg";
 function preload(){
- getBackgroundImage();
- polygon_img=loadImage("images/polygon.png");
- 
+getBackgroundImage();
+polygon_img=loadImage("polygon.png"); 
 }
 function setup() {
  createCanvas(900,400);
@@ -62,16 +61,15 @@ function setup() {
  blocks9 = new Block(700,95,30,40);
 
  //polygon holder with slings
-polygon_img=loadImage("polygon.png"); 
+polygon = Bodies.circle(50,200,20); 
  World.add(world,polygon);
- 
+
  slingShot = new Slingshot(this.polygon,{x:100,y:200});
 
 }
 function draw() {
  //background(56,44,44); 
- if(backgroundImg)
- background(backgroundImg);
+ background("black") 
  //Engine.update(engine);
  text(mouseX + ',' + mouseY, 10, 15);
  textSize(20);
